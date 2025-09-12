24ec763f04855d55439c2e1c943f0553e8a6226f
voltaire-rabino00 <voltairerabino9@gmail.com>
2025-09-13 01:01:44 +0800

    Initial Commit

--- script.js
+++ script.js
@@ -17,6 +17,24 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
+const weapons = [
+  {
+    name: "stick",
+    power: 5
+  },
+  {
+    name: "dagger",
+    power: 30
+  },
+  {
+    name: "claw hammer",
+    power: 50
+  },
+   {
+    name: "sword",
+    power: 100
+   }
+];
 const locations = [
   {
     name: "town square",
@@ -67,14 +85,24 @@ function goCave() {
 
 function fightDragon() {}
 
+
 function buyHealth() {
-    gold = gold -= 10;
-    health = health += 10;
+   if (gold >= 10) {
+    gold -= 10;
+    health += 10;
     goldText.innerText = gold;
     healthText.innerText = health;
+     } else {
+      text.innerText = "You do not have enough gold to buy health."
+     }
 }
 
-function buyWeapon() {}
+function buyWeapon() {
+  if (gold >= 30) {
+    gold -= 30;
+
+  }
+}
 
 function fightSlime () {
 
