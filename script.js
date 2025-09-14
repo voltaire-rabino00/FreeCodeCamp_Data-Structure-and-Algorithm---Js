90c33de88c3cfbd67ff8321ad2113f1d2f06a152
voltaire-rabino00 <voltairerabino9@gmail.com>
2025-09-14 23:22:07 +0800

    Initial Commit

--- script.js
+++ script.js
@@ -98,20 +98,20 @@ function buyHealth() {
 }
 
 function buyWeapon() {
-  if (gold >= 30) {
-    gold -= 30;
-    currentWeaponIndex ++;
-    goldText.innerText = gold;
-    let newWeapon = weapons[currentWeaponIndex].name;
-    text.innerText = "You now have a "+ newWeapon +".";
-    inventory.push(newWeapon);
-    text.innerText += " In your inventory you have: " + inventory;
+  if (currentWeaponIndex < 3) {
+    if (gold >= 30) {
+      gold -= 30;
+      currentWeaponIndex++;
+      let newWeapon = weapons[currentWeaponIndex].name;
+      inventory.push(newWeapon);
+      goldText.innerText = gold;
+      text.innerText = "You now have a " + newWeapon + ".";
+     } else {
+      text.innerText = "You do not have enough gold to buy a weapon."
+     }  
   }
   else {
-    text.innerText = "You do not have enough gold to buy a weapon.";
-  }
-  if (currentWeaponIndex < 3) {
-
+    text.innerText = "You already have the most powerful weapon!"
   }
 }
 
