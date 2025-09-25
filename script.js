a713bf06b93b0feec192450790dbcdc8ea3f9992
voltaire-rabino00 <voltairerabino9@gmail.com>
2025-09-25 22:08:42 +0800

    Initial Commit

--- script.js
+++ script.js
@@ -175,7 +175,6 @@ goFight();
 function goFight() {
 update(locations[3]);
 monsterHealth = monsters[fighting].health;
-const monsterStats = documetn.querySelector('p');
 monsterStats.style.display = 'block';
 }
 
