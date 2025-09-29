ac33be0de1bab5c32150f0da881f70eae01bf5e3
voltaire-rabino00 <voltairerabino9@gmail.com>
2025-09-29 20:32:10 +0800

    initial Commit

--- script.js
+++ script.js
@@ -176,10 +176,12 @@ function goFight() {
 update(locations[3]);
 monsterHealth = monsters[fighting].health;
 monsterStats.style.display = 'block';
+monsterName.innerText = monsters[fighting].name;
+monsterHealthText.innerText = monsterHealth;
 }
 
 function attack() {
-
+text.innerText = "The " + monsters[fighting].name + " attacks.";
 }
 
 function dodge () {
