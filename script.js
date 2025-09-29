f7b1b6e6c9f7307ef8e1a1544328079524155733
voltaire-rabino00 <voltairerabino9@gmail.com>
2025-09-29 21:18:44 +0800

    Initial Commit

--- script.js
+++ script.js
@@ -182,6 +182,10 @@ monsterHealthText.innerText = monsterHealth;
 
 function attack() {
 text.innerText = "The " + monsters[fighting].name + " attacks.";
+text.innerText += " You attack it with your " + weapons[currentWeaponIndex].name + ".";
+health -= monsters[fighting].level;
+monsterHealth -= weapons[currentWeaponIndex].power;
+
 }
 
 function dodge () {
